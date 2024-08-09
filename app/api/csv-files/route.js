f37fa34ddd62e7283d917f15af78bccbdaab02e5
Harsh <harshchamola@gmail.com>
2024-08-09 10:26:29 +0400

    final

--- app/api/csv-files/route.js
+++ app/api/csv-files/route.js
@@ -6,6 +6,7 @@ export const GET = async () => {
   try {
     await connectToDB();
     const csvFiles = await CsvFile.find();
+    console.log("CSV Files Retrieved: ", csvFiles);
     return NextResponse.json(csvFiles, { status: 200 });
   } catch (error) {
     console.error("Error fetching CSV files:", error);
--- components/Form.jsx
+++ components/Form.jsx
@@ -37,7 +37,7 @@ const Form = () => {
 
   const fetchCsvFiles = async () => {
     try {
-      const response = await fetch(`/api/csv-files`);
+      const response = await fetch(`/api/csv-files`, { cache: 'no-store' });
       const data = await response.json();
       if (response.ok) {
         setCsvFiles(data);
